511a38faf9ec40fc5f6e84e224b7271ce4a9cc1a
Paul Trowbridge <paul@hptrow.me>
2020-10-22 11:42:50 -0400

    prevent inserting null keys, and conflicting keys which is possible with disperate master data (maybe use max?)

--- sql/build_master_tables.sql
+++ sql/build_master_tables.sql
@@ -12,7 +12,7 @@ BEGIN
             string_agg(cname || ' ' || dtype,', ' ORDER BY opos ASC) || 
             ', PRIMARY KEY ('||string_agg(cname,', ') FILTER (WHERE fkey = func)||'));' AS ddl,
             ---need to add a clause to exclude where the key is null
-            'INSERT INTO fc.'||func||' SELECT DISTINCT ' || string_agg(cname,', ' ORDER BY opos ASC) || ' FROM rlarp.osm_dev' AS populate
+            'INSERT INTO fc.'||func||' SELECT DISTINCT ' || string_agg(cname,', ' ORDER BY opos ASC) || ' FROM rlarp.osm_dev WHERE '||string_agg(cname,'||') FILTER (WHERE fkey = func)||' IS NOT NULL ON CONFLICT DO NOTHING' AS populate
         FROM
             fc.target_meta
         WHERE
--- sql/temp.sql
+++ sql/temp.sql
@@ -1,11 +1,12 @@
-SELECT
-    'CREATE TABLE IF NOT EXISTS fc.'||func||' (' || 
-    string_agg(cname || ' ' || dtype,', ' ORDER BY opos ASC) || 
-    ', PRIMARY KEY ('||string_agg(cname,', ') FILTER (WHERE fkey = func)||'))' AS ddl,
-    'INSERT INTO fc.'||func||' SELECT DISTINCT ' || string_agg(cname,', ' ORDER BY opos ASC) || ' FROM fc.target' AS populate
-FROM
-    fc.target_meta
-WHERE
-    func <> 'doc'
-GROUP BY
-    func;
+        SELECT
+            'DROP TABLE IF EXISTS fc.'||func||'; CREATE TABLE IF NOT EXISTS fc.'||func||' (' || 
+            string_agg(cname || ' ' || dtype,', ' ORDER BY opos ASC) || 
+            ', PRIMARY KEY ('||string_agg(cname,', ') FILTER (WHERE fkey = func)||'));' AS ddl,
+            ---need to add a clause to exclude where the key is null
+            'INSERT INTO fc.'||func||' SELECT DISTINCT ' || string_agg(cname,', ' ORDER BY opos ASC) || ' FROM rlarp.osm_dev WHERE '||string_agg(cname,'||') FILTER (WHERE fkey = func)||' IS NOT NULL ON CONFLICT DO NOTHING' AS populate
+        FROM
+            fc.target_meta
+        WHERE
+            func <> 'doc'
+        GROUP BY
+            func
