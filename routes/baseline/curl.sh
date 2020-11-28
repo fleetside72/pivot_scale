08aa6ad6783267c063c9a24adc8a8bcd75e368d0
Paul Trowbridge <paul@hptrow.me>
2020-11-28 01:33:11 -0500

    update request body location

--- routes/baseline/curl.sh
+++ routes/baseline/curl.sh
@@ -1,1 +1,1 @@
-curl -H "Content-Type: application/json" -X GET -d@./routes/baseline/req.json https://localhost:8082/baseline --insecure > baseline.sql
+curl -H "Content-Type: application/json" -X GET -d@./routes/baseline/req.json https://localhost:8082/baseline --insecure > baseline
